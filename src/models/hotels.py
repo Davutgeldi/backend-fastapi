d113fadbdc2a597ea19da3b16f4849733897ca2e
Davutgeldi <davutsh06@gmail.com>
2024-09-25 17:48:08 +0500

    created model for hotels

---
+++ src/models/hotels.py
@@ -0,0 +1,12 @@
+from sqlalchemy.orm import Mapped, mapped_column
+from sqlalchemy import String
+
+from src.database import BaseModel
+
+
+class HotelsOrm(BaseModel):
+    __tablename__ = 'Hotels'
+
+    id: Mapped[int] = mapped_column(primary_key=True)
+    city: Mapped[str] = mapped_column(String(50))
+    name: Mapped[str]
