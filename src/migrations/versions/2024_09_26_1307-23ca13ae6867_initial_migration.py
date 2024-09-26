3f11f344f8bde565fe2f229966c18ccf9a9134c7
Davutgeldi <davutsh06@gmail.com>
2024-09-26 13:13:01 +0500

    changed migration, modified alembic.ini conf, created new useful migrations

---
+++ src/migrations/versions/2024_09_26_1307-23ca13ae6867_initial_migration.py
@@ -0,0 +1,33 @@
+"""initial migration
+
+Revision ID: 23ca13ae6867
+Revises: 
+Create Date: 2024-09-26 13:07:32.591958
+
+"""
+
+from typing import Sequence, Union
+
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision: str = "23ca13ae6867"
+down_revision: Union[str, None] = None
+branch_labels: Union[str, Sequence[str], None] = None
+depends_on: Union[str, Sequence[str], None] = None
+
+
+def upgrade() -> None:
+    op.create_table(
+        "Hotels",
+        sa.Column("id", sa.Integer(), nullable=False),
+        sa.Column("city", sa.String(length=50), nullable=False),
+        sa.Column("name", sa.String(), nullable=False),
+        sa.PrimaryKeyConstraint("id"),
+    )
+
+
+def downgrade() -> None:
+    op.drop_table("Hotels")
