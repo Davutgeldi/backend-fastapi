2d1ed1c301390076d179c47a4e30e555c2dedbe8
Davutgeldi <davutsh06@gmail.com>
2024-09-26 10:39:41 +0500

    created model for rooms

---
+++ src/models/rooms.py
@@ -0,0 +1,15 @@
+from sqlalchemy.orm import Mapped, mapped_column
+from sqlalchemy import String, ForeignKey, Text
+
+from src.database import BaseModel
+
+
+class RoomsOrm(BaseModel):
+    __tablename__ = 'Rooms'
+
+    id: Mapped[int] = mapped_column(primary_key=True)
+    hotel_id: Mapped[int] = mapped_column(ForeignKey('hotels.id'))
+    name: Mapped[str] = mapped_column(String(50))
+    description: Mapped[str | None]
+    price: Mapped[int]
+    quantity: Mapped[int]
