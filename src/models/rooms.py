2e2adf22a49e628f3ded89c9223a76756a52aee1
Davutgeldi <davutsh06@gmail.com>
2024-09-26 10:44:57 +0500

    fixed mistake in rooms model and transfer model to database

--- src/models/rooms.py
+++ src/models/rooms.py
@@ -8,7 +8,7 @@ class RoomsOrm(BaseModel):
     __tablename__ = 'Rooms'
 
     id: Mapped[int] = mapped_column(primary_key=True)
-    hotel_id: Mapped[int] = mapped_column(ForeignKey('hotels.id'))
+    hotel_id: Mapped[int] = mapped_column(ForeignKey('Hotels.id'))
     name: Mapped[str] = mapped_column(String(50))
     description: Mapped[str | None]
     price: Mapped[int]
