e60f6eafa00b2e1d27e716bf4a05e02df6c6dd30
Davutgeldi <davutsh06@gmail.com>
2024-09-28 17:14:12 +0500

    changed post request

--- src/api/hotels.py
+++ src/api/hotels.py
@@ -1,7 +1,11 @@
 from fastapi import Query, Body, APIRouter
 
+from sqlalchemy import insert
+
 from schemas.hotels import Hotel, HotelPATCH
 from src.api.dependencies import PaginationDep
+from src.database import async_session
+from src.models.hotels import HotelsOrm
 
 router = APIRouter(prefix='/hotels', tags=['Hotels'])
 
@@ -38,18 +42,16 @@ def delete_hotel(hotel_id: int):
 
 
 @router.post('')
-def create_hotel(hotel_data: Hotel = Body(openapi_examples={
+async def create_hotel(hotel_data: Hotel = Body(openapi_examples={
     '1': {'summary': 'Russia', 'value': {'city': 'Sochi', 'name': 'Balkan'}},
     '2': {'summary': 'USA', 'value': {'city': 'Florida', 'name': 'Miami Beach'}}
 })):
-    global hotels
-    hotels.append(
-        {
-            'id': hotels[-1]['id'] + 1,
-            'title': hotel_data.city,
-            'name': hotel_data.name
-        }
-    )
+    async with async_session() as session:
+        add_hotel_stmt = insert(HotelsOrm).values(**hotel_data.model_dump())
+        # Debug, sql statement
+        print(add_hotel_stmt.compile(compile_kwargs={'literal_binds': True}))
+        await session.execute(add_hotel_stmt)
+        await session.commit()
     return {'status': 'Successfully posted'}
 
 
